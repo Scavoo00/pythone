84a9526b732e16d0a99364a1ffa87886a40ead62
Scavoo00 <lolloscavo@gmail.com>
2025-01-23 12:55:50 +0100

    Update imposte.py

--- imposte.py
+++ imposte.py
@@ -1,5 +1,6 @@
-SCAGLIONE1=20000
-SCAGLIONE2=50000
+SCAGLIONE1=27000
+SCAGLIONE2=40000
+SCAGLIONE3=53000
 TASSA_SC1=0.23
 TASSA_SC2=0.35
 TASSA_SC3=0.43
@@ -7,8 +8,11 @@ reddito = float(input ("Che reddito hai? ")) #inserimento reddito
 print("Il tuo reddito è ",reddito,"euro")
 if reddito <= SCAGLIONE1 :
     aliquota = reddito * TASSA_SC1
-if reddito <= SCAGLIONE2 :
-    aliquota = SCAGLIONE1 * TASSA_SC1 + (reddito - SCAGLIONE1)* TASSA_SC2
+    print("Le tue tasse sono ",aliquota,"euro")
 else:
-    aliquota = SCAGLIONE1 * TASSA_SC1 + (SCAGLIONE2-SCAGLIONE1)*TASSA_SC2 + (reddito - SCAGLIONE2) * TASSA_SC3
-print("Le tue tasse sono ",aliquota,"euro")
+    if reddito <= SCAGLIONE2 :
+        aliquota = SCAGLIONE1 * TASSA_SC1 + (reddito - SCAGLIONE1)* TASSA_SC2
+        print("Le tue tasse sono ",aliquota,"euro")
+    else:
+        aliquota = SCAGLIONE1 * TASSA_SC1 + (SCAGLIONE2-SCAGLIONE1)*TASSA_SC2 + (reddito - SCAGLIONE2) * TASSA_SC3
+        print("Le tue tasse sono ",aliquota,"euro")
