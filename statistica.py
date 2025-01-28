96f80a846703d4e7530b8fea7222878a5cfc36f7
Scavoo00 <lolloscavo@gmail.com>
2025-01-28 13:03:28 +0100

    Update statistica.py

--- statistica.py
+++ statistica.py
@@ -5,5 +5,8 @@ riga = input()
 while riga.isdigit() :
     voti.append(int(riga))
     riga = input()
+voti.remove(min(voti))
+voti.remove(max(voti))
+print("La media dei voti è", sum(voti)/len(voti))
 for voto in voti :
     print(voto)
